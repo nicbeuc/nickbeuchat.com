9d23889d3e1338d174d0b7085daf40586f3bfa36
Nick Beuchat <nick@makememodern.com>
2025-05-25 20:31:19 -0400

    begin setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import localFont from "next/font/local";
+import "./global.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const switzerFont = localFont({
+  src: [
+    {
+      path: './fonts/switzer-variable.woff2',
+      style: 'normal',
+    },
+    {
+      path: './fonts/switzer-variable-italic.woff2',
+      style: 'italic',
+    },
+  ]
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,8 +26,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={switzerFont.className}>
+      <body>
         {children}
       </body>
     </html>
