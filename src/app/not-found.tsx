d02fc92534dd81e5620f36be98615593753d1321
Nick Beuchat <nick@makememodern.com>
2025-06-05 22:32:27 -0400

    some about page stuff

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ import './not-found.css'
 export default function NotFound() {
   return (
     <div className="not-found-wrapper">
-      <h1 className="font-body">404</h1>
+      <h1>404</h1>
       <p>Couldn&apos;t find that page.</p>
       <Link href="/">Return Home</Link>
     </div>
--- src/sections/AboutSection/index.tsx
+++ src/sections/AboutSection/index.tsx
@@ -1,9 +1,37 @@
+import Link from '@/components/Link'
 import styles from './styles.module.css'
 
 export default function AboutSection() {
   return (
     <div className={styles.wrapper}>
+      <section className={styles.imagesWrapper}>
 
+      </section>
+      <section className={styles.copyWrapper}>
+        <div data-layout="1">
+          <h2>About Me</h2>
+          <div className={styles.content}>
+            <p>Hi, I&apos;m Nick, a designer and creative developer with a passion for crafting simple, inclusive solutions for the web.</p>
+            <p>Currently, I&apos;m living and working in my hometown of Knoxville, TN, designing and developing user experiences for Make Me Modern.</p>
+          </div>
+        </div>
+        <div data-layout="1">
+          <h2>Contact</h2>
+          <div className={styles.content}>
+            <ul className={styles.linkList}>
+              <li>
+                <Link href="#" iconName="download" external>Resumé</Link>
+              </li>
+              <li>
+                <Link href="#" iconName="copy" external>Email</Link>
+              </li>
+              <li>
+                <Link href="#" iconName="arrow-top-right" external>LinkedIn</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
