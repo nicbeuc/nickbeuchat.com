6633bcf270f6d0532cdd71ef3fbf757fe5690531
Nick Beuchat <nick@makememodern.com>
2025-06-18 22:30:55 -0400

    colophon additions

--- src/sections/ColophonSection/index.tsx
+++ src/sections/ColophonSection/index.tsx
@@ -5,12 +5,24 @@ export default function ColophonSection() {
   return (
     <div className={styles.wrapper}>
       <section>
-        <h2>Icons</h2>
-        <p>This website features <Link external href="https://basicons.xyz/">Basicons</Link>, a simple and sharp icon suite created by <Link external href="https://www.preciousm.co/">Precious Madubuike</Link> and <Link external href="https://www.linkedin.com/in/solomon-omojola-6255198a/">Solomon Omojola.</Link></p>
+        <h2>Technical</h2>
+        <p>This site is built with Next.JS and hosted on Vercel. No AI was used in the creation of this site.</p>
       </section>
       <section>
         <h2>Accessibility</h2>
         <p>I&apos;m wholly committed to ensuring digital accessibility for any and all people who come across this site. If there&apos;s something you feel is preventing you from properly navigating the site or viewing its content, you can let me know <Link href="mailto:hi@nickbeuchat.com?subject=Accessibility%20Concern">via email</Link> and I will do my best to address it.</p>
+      </section>
+      <section>
+        <h2>Typography</h2>
+        <p>This site is set in <Link external href="https://www.fontshare.com/fonts/switzer">Switzer</Link>, a neo-grotesk typeface created by the incredible <Link external href="https://www.indiantypefoundry.com/">Indian Type Foundry</Link>.</p>
+      </section>
+      <section>
+        <h2>Icons</h2>
+        <p>This website features <Link external href="https://basicons.xyz/">Basicons</Link>, a simple and sharp icon suite created by <Link external href="https://www.preciousm.co/">Precious Madubuike</Link> and <Link external href="https://www.linkedin.com/in/solomon-omojola-6255198a/">Solomon Omojola.</Link></p>
+      </section>
+      <section>
+        <h2>Privacy</h2>
+        <p>I don&apos;t currently use any tracking cookies on this site, nor do I plan on doing so. Your privacy is important to me!</p>
       </section>
     </div>
   )
